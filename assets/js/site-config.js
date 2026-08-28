bd1ac1cc297cbc512ef3e6d7055dce93e3ba03f0
chaerim <chaerim@idblabs-MacBook-Air.local>
2026-08-28 11:51:54 +0900

    style: pfp 수정

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -15,7 +15,7 @@ window.SITE_CONFIG = {
   // 프로필 사진 여러 장 — 순서대로 나열하면 사이드바에서 옆으로 스크롤(스와이프)
   // 해서 넘겨볼 수 있는 캐러셀이 됩니다(assets/js/sidebar.js가 그림). 사진을
   // 하나만 쓰고 싶으면 배열에 경로 하나만 남기면 예전처럼 동작합니다.
-  avatarSrcs: ['/assets/img/rem.jpg', '/assets/img/miku2.jpg', '/assets/img/bocchi.jpg'],
+  avatarSrcs: ['/assets/img/rem.jpg', '/assets/img/sakuya.jpg', '/assets/img/bocchi.jpg'],
   avatarAlt: 'リミ 프로필 사진',
 
   copyright: '© miku-tenshi.github.io',
